207f0f4eb98757d3378ee0e9715f9eae5321f19a
Jannik Loth <jannik.loth@gmx.de>
2024-03-19 14:41:18 +0100

    delete get_proban_by_name()

--- app/db/crud.py
+++ app/db/crud.py
@@ -11,10 +11,6 @@ def get_proband_by_id(proband_id):
     return db.session.query(Proband).filter(Proband.id == proband_id).first()
 
 
-def get_proband_by_name(name):
-    return db.session.query(Proband).filter(Proband.name == name).first()
-
-
 def create_proband(_firstName, _lastName, _email, _gender, _birthday, _height, _weight, _health, _isActive):
     proband = Proband(firstName=_firstName, lastName=_lastName, email=_email, gender=_gender, birthday=_birthday,
                       height=_height, weight=_weight, health=_health, isActive=_isActive)
