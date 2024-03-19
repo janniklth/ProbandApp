6112b7c95992737adc08c6c5e559345d988d0c96
Jannik Loth <jannik.loth@gmx.de>
2024-03-19 11:05:14 +0100

    create pydantic schemas

--- app/schemas/gender.py
+++ app/schemas/gender.py
@@ -1,3 +1,15 @@
-from marshmallow import Schema, fields
+from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
 
+class GenderBase(BaseModel):
+    name: str
 
+class GenderCreate(GenderBase):
+    pass
+
+class Gender(GenderBase):
+    id: UUID
+
+    class Config:
+        orm_mode = True
--- app/schemas/proband.py
+++ app/schemas/proband.py
@@ -1,1 +1,24 @@
-from marshmallow import Schema, fields
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class ProbandBase(BaseModel):
+    firstname: str
+    lastname: str
+    email: str
+    gender_id: int
+    birthday: date
+    weight: float
+    height: float
+    health: float
+    country_id: int
+    isactive: bool
+
+class ProbandCreate(ProbandBase):
+    pass
+
+class Proband(ProbandBase):
+    id: int
+
+    class Config:
+        orm_mode = True
